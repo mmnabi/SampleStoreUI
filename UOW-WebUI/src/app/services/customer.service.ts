754b29b3e63f5f6d6623814e2f274241369c6598
mmnabi <mmnabi.cse@gmail.com>
2018-05-31 14:20:06 +0600

    Snackbar added.

--- UOW-WebUI/src/app/services/customer.service.ts
+++ UOW-WebUI/src/app/services/customer.service.ts
@@ -9,6 +9,8 @@ import { Observable, of } from 'rxjs';
 
 import { catchError, map, tap } from 'rxjs/operators';
 
+import { MatSnackBar } from '@angular/material';
+
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -20,13 +22,38 @@ const httpOptions = {
 export class CustomerService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private snackBar: MatSnackBar
   ) { }
 
   private customersUrl = 'http://localhost/UOW/api/customers/';
 
   getCustomers(): Observable<Customer[]> {
     // return of(CUSTOMERS);
-    return this.http.get<Customer[]>(this.customersUrl, httpOptions);
+    return this.http.get<Customer[]>(this.customersUrl, httpOptions)
+    .pipe(
+      tap(heroes => this.openSnackBar('Customers Loaded.')),
+      catchError(this.handleError('getHeroes', []))
+    );
+  }
+
+  openSnackBar(message: string): void {
+    this.snackBar.open(message, "Ok", {
+      duration: 2000,
+    });
+  };
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.openSnackBar(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
